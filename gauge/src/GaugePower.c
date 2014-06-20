edb6aa3f7f24ed97bd42f309c2da68a9d70ca4e7
ukchkn <chris.knight@teligent.co.uk>
2014-06-20 14:39:36 +0100

    Read the power in XML format

--- gauge/src/GaugePower.c
+++ gauge/src/GaugePower.c
@@ -26,6 +26,8 @@
 #include <time.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
 
 #include "config.h"
 #include "socket.h"
@@ -68,6 +70,111 @@ void readPowerMeterInit (void)
 
 /**********************************************************************************************************************
  *                                                                                                                    *
+ *  P R O C E S S  P O W E R  K E Y                                                                                   *
+ *  ===============================                                                                                   *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
+/**
+ *  @brief Process each of the fields in the XML.
+ *  @param readLevel 0 current, 1 today, 2 tomorrow.
+ *  @param name Name of the field.
+ *  @param value Value of the field.
+ *  @result None.
+ */
+static void processPowerKey (int readLevel, const char *name, char *value)
+{
+	if (readLevel == 1 && strcmp (name, "now") == 0)
+		myPowerReading[0] = atof (value);
+	if (readLevel == 1 && strcmp (name, "max") == 0)
+		myPowerReading[1] = atof (value);
+	if (readLevel == 1 && strcmp (name, "min") == 0)
+		myPowerReading[2] = atof (value);
+	if (readLevel == 1 && strcmp (name, "minavg") == 0)
+		myPowerReading[3] = atof (value);
+	if (readLevel == 1 && strcmp (name, "houravg") == 0)
+		myPowerReading[4] = atof (value);
+	if (readLevel == 1 && strcmp (name, "dayavg") == 0)
+		myPowerReading[5] = atof (value);
+	if (readLevel == 1 && strcmp (name, "monthavg") == 0)
+		myPowerReading[6] = atof (value);		
+	if (readLevel == 1 && strcmp (name, "yearavg") == 0)
+		myPowerReading[7] = atof (value);
+}
+
+/**********************************************************************************************************************
+ *                                                                                                                    *
+ *  P R O C E S S  E L E M E N T  N A M E S                                                                           *
+ *  =======================================                                                                           *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
+/**
+ *  @brief Process each of the elements in the file.
+ *  @param doc Document to read.
+ *  @param aNode Current node.
+ *  @param readLevel 0 current, 1 today, 2 tomorrow.
+ *  @result None.
+ */
+static void processElementNames (xmlDoc *doc, xmlNode * aNode, int readLevel)
+{
+	xmlChar *key;
+    xmlNode *curNode = NULL;
+
+    for (curNode = aNode; curNode; curNode = curNode->next) 
+    {
+        if (curNode->type == XML_ELEMENT_NODE) 
+        {
+			if ((!xmlStrcmp (curNode -> name, (const xmlChar *)"power"))) 
+			{
+				++readLevel;
+			}
+			else
+			{
+				key = xmlNodeListGetString (doc, curNode -> xmlChildrenNode, 1);
+				processPowerKey (readLevel, (const char *)curNode -> name, (char *)key);
+		    	xmlFree (key);
+		    }
+        }
+        processElementNames (doc, curNode->children, readLevel);
+    }
+}
+
+/**********************************************************************************************************************
+ *                                                                                                                    *
+ *  P R O C E S S  B U F F E R                                                                                        *
+ *  ==========================                                                                                        *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
+/**
+ *  @brief Process the down loaded buffer.
+ *  @param buffer Buffer to process.
+ *  @param size Size of the buffer.
+ *  @result None.
+ */
+static void processBuffer (char *buffer, size_t size)
+{
+	xmlDoc *doc = NULL;
+	xmlNode *rootElement = NULL;
+	xmlChar *xmlBuffer = NULL;
+
+	xmlBuffer = xmlCharStrndup (buffer, size);
+	doc = xmlParseDoc (xmlBuffer);
+
+	if (doc != NULL)
+	{
+		rootElement = xmlDocGetRootElement (doc);
+		processElementNames (doc, rootElement, 0);
+		xmlFreeDoc (doc);
+	}
+	else
+	{
+		printf ("error: could not parse memory\n");
+		printf ("BUFF[%d] [[[%s]]]\n", size, buffer);
+	}
+	xmlCleanupParser ();
+}
+
+/**********************************************************************************************************************
+ *                                                                                                                    *
  *  R E A D  P O W E R  M E T E R  I N F O                                                                            *
  *  ======================================                                                                            *
  *                                                                                                                    *
@@ -78,7 +185,7 @@ void readPowerMeterInit (void)
  */
 void readPowerMeterInfo ()
 {
-	char buffer[256] = "", addr[20];
+	char buffer[512] = "", addr[20];
 	int clientSock = -1, bytesRead = 0;
 	
 	if (GetAddressFromName (powerServer, addr))
@@ -87,37 +194,12 @@ void readPowerMeterInfo ()
 	}
 	if (SocketValid (clientSock))
 	{
-		bytesRead = RecvSocket (clientSock, buffer, 255);
+		bytesRead = RecvSocket (clientSock, buffer, 511);
 		CloseSocket (&clientSock);
 	}
 	if (bytesRead)
 	{
-		char string[41] = "";
-		int i = 0, let = 0, field = 0;
-
-		buffer[bytesRead] = 0;
-		while (buffer[i])
-		{
-			if ((buffer[i] >= '0' && buffer[i] <= '9') || buffer[i] == '.' || buffer[i] == '-')
-			{
-				string[let] = buffer[i];
-				string[++let] = 0;
-			}
-			else if (buffer[i] == ':')
-			{
-				field = atoi (string);
-				string[let = 0] = 0;
-			}
-			else if (buffer[i] == ',')
-			{
-				if (field < 8)
-				{
-					myPowerReading[field] = atof (string);
-				}
-				string[let = 0] = 0;
-			}
-			++i;
-		}		
+		processBuffer (buffer, bytesRead);
 	}
 }
 
