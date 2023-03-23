09f5fc3804e000c7f0b573dd7ac11eeeace30418
Chris Knight <chris.knight@teligent.co.uk>
2023-03-23 08:47:16 +0000

    Changed to GPLv3 headers

--- gauge/src/GaugeWeather.c
+++ gauge/src/GaugeWeather.c
@@ -3,17 +3,18 @@
  *  G A U G E  W E A T H E R . C                                                                                      *
  *  ============================                                                                                      *
  *                                                                                                                    *
- *  This is free software; you can redistribute it and/or modify it under the terms of the GNU General Public         *
- *  License version 2 as published by the Free Software Foundation.  Note that I am not granting permission to        *
- *  redistribute or modify this under the terms of any later version of the General Public License.                   *
+ *  Copyright (c) 2023 Chris Knight                                                                                   *
  *                                                                                                                    *
- *  This is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied        *
+ *  File GaugeWeather.c part of Gauge is free software: you can redistribute it and/or modify it under the terms of   *
+ *  the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or  *
+ *  (at your option) any later version.                                                                               *
+ *                                                                                                                    *
+ *  Gauge is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied       *
  *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more     *
  *  details.                                                                                                          *
  *                                                                                                                    *
- *  You should have received a copy of the GNU General Public License along with this program (in the file            *
- *  "COPYING"); if not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111,   *
- *  USA.                                                                                                              *
+ *  You should have received a copy of the GNU General Public License along with this program. If not, see:           *
+ *  <http://www.gnu.org/licenses/>                                                                                    *
  *                                                                                                                    *
  **********************************************************************************************************************/
 /**
